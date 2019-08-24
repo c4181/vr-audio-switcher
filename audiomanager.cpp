bec80dcb85bbbe26a26f55e698f8e55f002b2ab7
c4181 <cm4181@gmail.com>
2019-08-24 03:52:53 -0400

    New audiomanager based on dam

---
+++ audiomanager.cpp
@@ -0,0 +1,6 @@
+#include "audiomanager.h"
+
+audiomanager::audiomanager()
+{
+
+}
---
+++ audiomanager.h
@@ -0,0 +1,11 @@
+#ifndef AUDIOMANAGER_H
+#define AUDIOMANAGER_H
+
+
+class audiomanager
+{
+public:
+    audiomanager();
+};
+
+#endif // AUDIOMANAGER_H
