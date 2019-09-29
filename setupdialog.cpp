69ea0c3b9b340e956562073419f45d10e6d433b8
unknown <bluewaveck777@yahoo.com>
2019-09-28 23:22:38 -0500

    Save audio devices to a settings file
    Settings will now save to, and load from, a file called "audio_settings.ini".
    The file will be located in the program's current working directory.

--- setupdialog.cpp
+++ setupdialog.cpp
@@ -1,6 +1,8 @@
 #include "setupdialog.h"
 #include "ui_setupdialog.h"
 
+#define SETTINGS_FILE_NAME "audio_settings.ini"
+
 SetupDialog::SetupDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SetupDialog) {
@@ -10,6 +12,7 @@ SetupDialog::SetupDialog(QWidget *parent) :
     ui->desktop_playback_combo_box->addItems(playback_device_list_);
     ui->vr_recording_combo_box->addItems(recording_device_list_);
     ui->desktop_recording_combo_box->addItems(recording_device_list_);
+    SelectSavedDevices();
 }
 
 SetupDialog::~SetupDialog() {
@@ -20,6 +23,37 @@ SetupDialog::~SetupDialog() {
 // to populate the Ui comboboxes
 void SetupDialog::ListAudioDevices() {
     AudioManager audio_devices;
+    ifstream settings_file (SETTINGS_FILE_NAME);
+    string current_line;
+    vector<string> settings_buffer;
+
+    if(settings_file.is_open())
+    {
+        while (getline(settings_file, current_line))
+        {
+            settings_buffer.push_back(current_line);
+        }
+        settings_file.close();
+        for(ulong i=0;i<settings_buffer.size();++i)
+        {
+            if(settings_buffer[i] == "[vr_record]")
+            {
+                saved_vr_record_selection = settings_buffer[i+1];
+            }
+            if(settings_buffer[i] == "[vr_playback]")
+            {
+                saved_vr_playback_selection = settings_buffer[i+1];
+            }
+            if(settings_buffer[i] == "[desktop_record]")
+            {
+                saved_desktop_record_selection = settings_buffer[i+1];
+            }
+            if(settings_buffer[i] == "[desktop_playback]")
+            {
+                saved_desktop_playback_selection = settings_buffer[i+1];
+            }
+        }
+    }
 
     playback_devices_ = audio_devices.GetPlaybackDevices();
     recording_devices_ = audio_devices.GetRecordingDevices();
@@ -103,4 +137,63 @@ AudioDevice SetupDialog::GetDesktopRecordingDevice() {
 void SetupDialog::ErrorFindingDevice(QString device) {
     QMessageBox::critical(this, "Error Finding Device", "Error: Could not find " + device, QMessageBox::Ok, QMessageBox::Ok);
     QCoreApplication::exit(1);
+}
+
+void SetupDialog::SelectSavedDevices() {
+    if (!saved_vr_record_selection.empty())
+    {
+        int index = ui->vr_recording_combo_box->findText(QString::fromStdString(saved_vr_record_selection));
+        if(index != -1)
+        {
+            ui->vr_recording_combo_box->setCurrentIndex(index);
+        }
+    }
+    if (!saved_vr_playback_selection.empty())
+    {
+        int index = ui->vr_playback_combo_box->findText(QString::fromStdString(saved_vr_playback_selection));
+        if(index != -1)
+        {
+            ui->vr_playback_combo_box->setCurrentIndex(index);
+        }
+    }
+    if (!saved_desktop_record_selection.empty())
+    {
+        int index = ui->desktop_recording_combo_box->findText(QString::fromStdString(saved_desktop_record_selection));
+        if(index != -1)
+        {
+            ui->desktop_recording_combo_box->setCurrentIndex(index);
+        }
+    }
+    if (!saved_desktop_playback_selection.empty())
+    {
+        int index = ui->desktop_playback_combo_box->findText(QString::fromStdString(saved_desktop_playback_selection));
+        if(index != -1)
+        {
+            ui->desktop_playback_combo_box->setCurrentIndex(index);
+        }
+    }
+}
+
+void SetupDialog::save_on_dialog_accept() {
+    string svr_record_selection = ui->vr_recording_combo_box->currentText().toStdString();
+    string svr_playback_selection = ui->vr_playback_combo_box->currentText().toStdString();
+    string sdesktop_record_selection = ui->desktop_recording_combo_box->currentText().toStdString();
+    string sdesktop_playback_selection = ui->desktop_playback_combo_box->currentText().toStdString();
+    ofstream settings_file(SETTINGS_FILE_NAME, std::ios::out | std::ios::trunc);
+
+    if(settings_file.is_open())
+    {
+        settings_file << "[vr_record]\n";
+        settings_file << svr_record_selection << "\n";
+        settings_file << "[vr_playback]\n";
+        settings_file << svr_playback_selection << "\n";
+        settings_file << "[desktop_record]\n";
+        settings_file << sdesktop_record_selection << "\n";
+        settings_file << "[desktop_playback]\n";
+        settings_file << sdesktop_playback_selection << "\n";
+        settings_file.close();
+    } else
+    {
+        QMessageBox::information(this, "Unable to Save Settings", "Unable to save settings to settings file.\nYour devices will reset to default on next launch.");
+    }
 }
--- setupdialog.h
+++ setupdialog.h
@@ -6,12 +6,16 @@
 #include <QStringList>
 #include <QString>
 #include <string>
+#include <iostream>
+#include <fstream>
 
 #include "audiomanager.h"
 
 using std::string;
 using std::wstring;
 using std::vector;
+using std::ofstream;
+using std::ifstream;
 
 namespace Ui {
 class SetupDialog;
@@ -33,11 +37,19 @@ private:
     Ui::SetupDialog *ui;
     void ListAudioDevices();
     void ErrorFindingDevice(QString);
+    void SelectSavedDevices();
     vector<AudioDevice> playback_devices_;
     vector<AudioDevice> recording_devices_;
     QStringList playback_device_list_;
     QStringList recording_device_list_;
     QString ConvertString(wstring);
+    string saved_vr_record_selection;
+    string saved_vr_playback_selection;
+    string saved_desktop_record_selection;
+    string saved_desktop_playback_selection;
+
+private slots:
+    void save_on_dialog_accept();
 };
 
 #endif // SETUPDIALOG_H
